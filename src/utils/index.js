b2f9338f3c14242fe8323bd00685434425cf8268
MatsRahmat <matsrahmathidayat@gmail.com>
2024-07-15 20:35:09 +0700

    delete everything

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-export function typedTing() {
-  const typed = new Typed('.mutiple-text', {
-    string: ["Sofware Developer", "Frontend Developer", "Backend Developer", "Fullstack Developer", "Mobile Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    typeDelay: 1000,
-    loop: true
-  })
-}
